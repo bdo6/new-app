dd83caabeee88ebe6ada269a05fe7cf48d532aaa
Brian <b3d4o5@Ggmail.com>
2021-01-25 12:47:59 -0800

    hi

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+function App() {
+  return <div className="App">
+    <header className="header">
+      <div className="logo" />
+      MyFirstApp
+    </header>
+  </div>
+}
+export default App;
